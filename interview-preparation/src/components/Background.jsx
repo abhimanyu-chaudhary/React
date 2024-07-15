330873264b73eb32f617c18e11815cea1dec4f48
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-07-15 23:05:14 +0545

    Update Background.jsx

--- interview-preparation/src/components/Background.jsx
+++ interview-preparation/src/components/Background.jsx
@@ -5,25 +5,25 @@ function Background() {
 
     const colorHandler = (color) => {
         switch (color){
-            case Red:
-                return backColor = "bg-red-500";
+            case 1:
+                setBackColor("red");
                 break;
             
-            case Blue:
-                backColor = "bg-blue-500";
+            case 2:
+                setBackColor("blue");
                 break;
         }
     }
   return (
-    <div style={{backgroundColor: backColor }} className='w-screen h-screen'>
+    <div  className={`w-screen h-screen ${backColor}`}>
         <div className='max-w-screen-lg mx-auto flex justify-center gap-4 pt-[50vh]'>
-        <button className='px-3 py-2 rounded-lg border-2 min-w-20' onClick={()=> colorHandler(Red)}>Red</button>
-        <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Blue</button>
-        <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Yellow</button>
-        <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Orange</button>
-        <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Black</button>
-        <button className='px-3 py-2 rounded-lg border-2 min-w-20'>White</button>
-        <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Pink</button>
+        <button className='px-3 py-2 rounded-lg border-2 min-w-20' onClick={()=> colorHandler(1)}>Red</button>
+        <button className='px-3 py-2 rounded-lg border-2 min-w-20' onClick={()=> colorHandler(2)}>Blue</button>
+        <button className='px-3 py-2 rounded-lg border-2 min-w-20' onClick={()=> colorHandler(1)}>Yellow</button>
+        <button className='px-3 py-2 rounded-lg border-2 min-w-20' onClick={()=> colorHandler(1)}>Orange</button>
+        <button className='px-3 py-2 rounded-lg border-2 min-w-20' onClick={()=> colorHandler(1)}>Black</button>
+        <button className='px-3 py-2 rounded-lg border-2 min-w-20' onClick={()=> colorHandler(1)}>White</button>
+        <button className='px-3 py-2 rounded-lg border-2 min-w-20' onClick={()=> colorHandler(1)}>Pink</button>
         </div>
     </div>
   )
