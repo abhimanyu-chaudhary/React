d52d50bd30982d909ab3dabdcbd0e4a6002c3680
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-07-15 22:48:52 +0545

    Update Background.jsx

--- interview-preparation/src/components/Background.jsx
+++ interview-preparation/src/components/Background.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Background() {
-    const colorHandler = () => {
-        switch 
+    const [backColor, setBackColor] = useState("white")
+
+    const colorHandler = (color) => {
+        switch (color){
+            case Red:
+                return backColor = "bg-red-500";
+                break;
+            
+            case Blue:
+                backColor = "bg-blue-500";
+                break;
+        }
     }
   return (
-    <div className='max-w-screen-lg mx-auto flex justify-center gap-4 mt-[50vh] -translate-y-[50%]'>
-      <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Red</button>
-      <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Blue</button>
-      <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Yellow</button>
-      <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Orange</button>
-      <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Black</button>
-      <button className='px-3 py-2 rounded-lg border-2 min-w-20'>White</button>
-      <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Pink</button>
+    <div style={{backgroundColor: backColor }} className='w-screen h-screen'>
+        <div className='max-w-screen-lg mx-auto flex justify-center gap-4 pt-[50vh]'>
+        <button className='px-3 py-2 rounded-lg border-2 min-w-20' onClick={()=> colorHandler(Red)}>Red</button>
+        <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Blue</button>
+        <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Yellow</button>
+        <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Orange</button>
+        <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Black</button>
+        <button className='px-3 py-2 rounded-lg border-2 min-w-20'>White</button>
+        <button className='px-3 py-2 rounded-lg border-2 min-w-20'>Pink</button>
+        </div>
     </div>
   )
 }
