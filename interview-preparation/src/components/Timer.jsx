47a339eefc8bd001624daf28361225b0f40c7153
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-07-16 21:53:57 +0545

    Update Timer.jsx

--- interview-preparation/src/components/Timer.jsx
+++ interview-preparation/src/components/Timer.jsx
@@ -4,8 +4,8 @@ function Timer() {
   return (
     <div className='flex flex-col gap-20 pt-20'>
       <div className='flex text-9xl font-semibold justify-center gap-4'>
-        <h1>0</h1> :
-        <h1>0</h1> :
+        <h1>1</h1> :
+        <h1>2</h1> :
         <h1>0</h1> 
       </div>
       <button className='mx-auto w-40 px-3 py-2 border-2 rounded-lg'>START</button>
