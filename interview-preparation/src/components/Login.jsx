1273cb36cae8f264bc657102edaae7befa04ef07
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-08-01 22:21:12 +0545

    Update Login.jsx

--- interview-preparation/src/components/Login.jsx
+++ interview-preparation/src/components/Login.jsx
@@ -5,13 +5,32 @@ function Login() {
   const [password, setPassword] = useState();
   const [isRegistered, setIsRegistered] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  const handleAuthentication = () => {
+    if(isRegistered){
+        if (isRegistered) {
+            const user = users.find((user) => user.email === email && user.password === password);
+            if(user){
+                setIsLoggedIn(true);
+            }else{
+                alert("Please check the credentials");
+            }
+        }
+    }else{
+        const newUser = {email, password};
+        setUsers([...users, newUser]);
+        localStorage.setItem('users', JSON.stringify([...users, newUser]));
+        setIsLoggedIn(true);
+    }
+  }
 
   return (
     <div className="py-[20%]">
       {isLoggedIn ? (
         <div className="text-3xl flex flex-col gap-10">
           <h1>Hello, {email}</h1>
-          <button className="px-4 py-1 bg-blue-500 h-10 text-white">Logout</button>
+          <button onClick={() => setIsLoggedIn(false)} className="px-4 py-2 bg-red-500 rounded-lg text-white">Logout</button>
         </div>
       ) : (
         <div>
@@ -20,16 +39,20 @@ function Login() {
               type="text"
               placeholder="Email Address"
               className="border-2 m-3 h-10"
+              onChange={(e) => setEmail(e.target.value)}
+              required
             />
             <input
               type="password"
               placeholder="Password"
               className="border-2 m-3 h-10"
+              onChange={(e) => setPassword(e.target.value)}
+              required
             />
             <button
               type="submit"
               className="px-4 py-1 bg-blue-500 h-10 text-white"
-              onClick={() => setIsLoggedIn(!isLoggedIn)}
+              onClick={handleAuthentication}
             >
               {isRegistered ? "Login" : "Register"}
             </button>
