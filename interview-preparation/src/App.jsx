5a790a09a25a4e315014117a514e180d9acc5972
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-08-06 19:56:39 +0545

    Update App.jsx

--- interview-preparation/src/App.jsx
+++ interview-preparation/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Accordion from "./components/Accordion";
 import Background from "./components/Background";
+import ColorPicker from "./components/ColorPicker";
 import Counter from "./components/Counter";
 import Fetch from "./components/FetchAPI";
 import FileUploader from "./components/FileUploader";
 import Input from "./components/Input";
 import List from "./components/List";
 import Login from "./components/Login";
+import Menu from "./components/Menu";
 import RandomQuotes from "./components/RandomQuotes";
 import SearchItem from "./components/SearchItem";
 import Timer from "./components/Timer";
@@ -28,7 +30,9 @@ function App() {
       {/* <FileUploader /> */}
       {/* <Login /> */}
       {/* <Weather /> */}
-      <SearchItem />
+      {/* <SearchItem /> */}
+      {/* <ColorPicker /> */}
+      <Menu />
     </div>
   );
 }
