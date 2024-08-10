621c2ddc2ba9f4e3bbf6fe649b70eed37bf360a3
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-08-10 21:38:02 +0545

    Update SocialMedia.jsx

--- interview-preparation/src/components/SocialMedia.jsx
+++ interview-preparation/src/components/SocialMedia.jsx
@@ -3,8 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 
 function SocialMedia() {
   return (
-    <div className='text-8xl font-extrabold'>
-      <h1 className='font-extrabold'>ABHIMANYU CHAUDHARY</h1>
+    <div className='text-8xl font-extrabold border-[2px] border-slate-950 p-5 rounded-md mt-[30vh]'>
+      <h1 className='font-extrabold '>ABHIMANYU CHAUDHARY</h1>
     </div>
   )
 }
