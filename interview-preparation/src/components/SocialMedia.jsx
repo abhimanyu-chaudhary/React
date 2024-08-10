728a5905ed38a9299aa07397fd3b0fa35273e4c5
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-08-10 22:02:26 +0545

    Update SocialMedia.jsx

--- interview-preparation/src/components/SocialMedia.jsx
+++ interview-preparation/src/components/SocialMedia.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom';
 
 function SocialMedia() {
+    let [firstVal, setFirstVal] = useState(0);
+    let [secondVal, setSecondVal] = useState(0);
+
+
+    useEffect(() =>{
+        setInterval(() => {
+            firstVal = Math.floor(Math.random() * 500);
+            secondVal = Math.floor(Math.random() * 500);
+            setFirstVal(firstVal);
+            setSecondVal(secondVal);
+        }, 2000);
+
+        clearInterval();
+    })
+
+    const letterMove = {
+        transform : `translate(${firstVal}px, ${secondVal}px)`
+    }
+    
+
   return (
-    <div className='text-8xl font-extrabold border-[2px] border-slate-950 p-5 rounded-md mt-[30vh]'>
+    <div className={`text font-extrabold border-[2px] border-slate-950 p-5 rounded-md mt-[30vh] select-none`} style={letterMove}>
       <h1 className='font-extrabold '>ABHIMANYU CHAUDHARY</h1>
     </div>
   )
